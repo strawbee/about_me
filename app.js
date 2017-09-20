e880db9323a5a1cda3467a813fb4d684f24c134e
strawbee <yuejiao.hou@gmail.com>
2017-09-20 09:46:29 -0700

    Simplifying JS

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ document.getElementById('username').innerHTML = userName;
 alert('Hello, ' + userName + '! Let\'s play a game to see how well you know Joy. Type \"yes\" or \"no\" to the following five questions.');
 
 // Question 1
-var answer1 = prompt('Does Joy like pineapples on pizza?');
+var answer1 = prompt('Does Joy like pineapples on pizza?').toUpperCase();
 
-while (answer1.toUpperCase() !== 'Y' && answer1.toUpperCase() !== 'YES' && answer1.toUpperCase() !== 'N' && answer1.toUpperCase() !== 'NO') {
-  answer1 = prompt('You did not answer in the correct format. Please answer with yes or no. Does Joy like pineapples on pizza?');
+while (answer1 !== 'Y' && answer1 !== 'YES' && answer1 !== 'N' && answer1 !== 'NO') {
+  answer1 = prompt('You did not answer in the correct format. Please answer with yes or no. Does Joy like pineapples on pizza?').toUpperCase();
 }
 
-if (answer1.toUpperCase() === 'N' || answer1.toUpperCase() === 'NO') {
+if (answer1 === 'N' || answer1 === 'NO') {
   alert('You\'re right, of course!');
   console.log(userName + ' guessed that Joy does not like pineapple on pizza. Correct!');
   pointsScored++;
@@ -30,16 +30,16 @@ else {
   console.log(userName + ' guessed that Joy likes pineapple on pizza. Gross!');
 }
 
-document.getElementById('q1result').innerHTML = answer1.toUpperCase();
+document.getElementById('q1result').innerHTML = answer1;
 
 // Question 2
-var answer2 = prompt('Does Joy have a dog?');
+var answer2 = prompt('Does Joy have a dog?').toUpperCase();
 
-while (answer2.toUpperCase() !== 'Y' && answer2.toUpperCase() !== 'YES' && answer2.toUpperCase() !== 'N' && answer2.toUpperCase() !== 'NO') {
-  answer2 = prompt('You did not answer in the correct format. Please answer with yes or no. Does Joy have a dog?');
+while (answer2 !== 'Y' && answer2 !== 'YES' && answer2 !== 'N' && answer2 !== 'NO') {
+  answer2 = prompt('You did not answer in the correct format. Please answer with yes or no. Does Joy have a dog?').toUpperCase();
 }
 
-if (answer2.toUpperCase() === 'Y' || answer2.toUpperCase() === 'YES') {
+if (answer2 === 'Y' || answer2 === 'YES') {
   alert('You\'re right, she does! She has a labradoodle named Toby.');
   console.log(userName + ' guessed correctly that Joy does have a dog.');
   pointsScored++;
@@ -49,16 +49,16 @@ else {
   console.log(userName + ' guessed that Joy does not have a dog. Wrong!');
 }
 
-document.getElementById('q2result').innerHTML = answer2.toUpperCase();
+document.getElementById('q2result').innerHTML = answer2;
 
 // Question 3
-var answer3 = prompt('Does Joy have a cat?');
+var answer3 = prompt('Does Joy have a cat?').toUpperCase();
 
-while (answer3.toUpperCase() !== 'Y' && answer3.toUpperCase() !== 'YES' && answer3.toUpperCase() !== 'N' && answer3.toUpperCase() !== 'NO') {
-  answer3 = prompt('You did not answer in the correct format. Please answer with yes or no. Does Joy have a cat?');
+while (answer3 !== 'Y' && answer3 !== 'YES' && answer3 !== 'N' && answer3 !== 'NO') {
+  answer3 = prompt('You did not answer in the correct format. Please answer with yes or no. Does Joy have a cat?').toUpperCase();
 }
 
-if (answer3.toUpperCase() === 'Y' || answer3.toUpperCase() === 'YES') {
+if (answer3 === 'Y' || answer3 === 'YES') {
   alert('You\'re right, she does! Her cat\'s is named Snowdrop.');
   console.log(userName + ' guessed correctly that Joy has a cat.');
   pointsScored++;
@@ -68,16 +68,16 @@ else {
   console.log(userName + ' guessed that Joy does not have a cat. Wrong!');
 }
 
-document.getElementById('q3result').innerHTML = answer3.toUpperCase();
+document.getElementById('q3result').innerHTML = answer3;
 
 // Question 4
-var answer4 = prompt('Was Joy born in a barn?');
+var answer4 = prompt('Was Joy born in a barn?').toUpperCase();
 
-while (answer4.toUpperCase() !== 'Y' && answer4.toUpperCase() !== 'YES' && answer4.toUpperCase() !== 'N' && answer4.toUpperCase() !== 'NO') {
-  answer4 = prompt('You did not answer in the correct format. Please answer with yes or no. Was Joy born in a barn?');
+while (answer4 !== 'Y' && answer4 !== 'YES' && answer4 !== 'N' && answer4 !== 'NO') {
+  answer4 = prompt('You did not answer in the correct format. Please answer with yes or no. Was Joy born in a barn?').toUpperCase();
 }
 
-if (answer4.toUpperCase() === 'N' || answer4.toUpperCase() === 'NO') {
+if (answer4 === 'N' || answer4 === 'NO') {
   alert('You\'re right! She was not born in a barn.');
   console.log(userName + ' guessed correctly that Joy was not born in a barn.');
   pointsScored++;
@@ -87,16 +87,16 @@ else {
   console.log(userName + ' is a big wrong jerk and guessed that Joy is born in a barn.');
 }
 
-document.getElementById('q4result').innerHTML = answer4.toUpperCase();
+document.getElementById('q4result').innerHTML = answer4;
 
 // Question 5
-var answer5 = prompt('Is Joy awesome?');
+var answer5 = prompt('Is Joy awesome?').toUpperCase();
 
-while (answer5.toUpperCase() !== 'Y' && answer5.toUpperCase() !== 'YES' && answer5.toUpperCase() !== 'N' && answer5.toUpperCase() !== 'NO') {
-  answer5 = prompt('You did not answer in the correct format. Please answer with yes or no. Is Joy awesome?');
+while (answer5 !== 'Y' && answer5 !== 'YES' && answer5 !== 'N' && answer5 !== 'NO') {
+  answer5 = prompt('You did not answer in the correct format. Please answer with yes or no. Is Joy awesome?').toUpperCase();
 }
 
-if (answer5.toUpperCase() === 'Y' || answer5.toUpperCase() === 'YES') {
+if (answer5 === 'Y' || answer5 === 'YES') {
   alert('Of course, you are correct.');
   console.log(userName + ' guessed correctly that Joy is awesome.');
   pointsScored++;
@@ -106,7 +106,7 @@ else {
   console.log(userName + ' thinks Joy is not awesome!! OMG.');
 }
 
-document.getElementById('q5result').innerHTML = answer5.toUpperCase();
+document.getElementById('q5result').innerHTML = answer5;
 
 
 // Displays points earned and a message accordingly
