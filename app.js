95c0434ccc5ba9515677b927dd9c919c385dea2e
strawbee <yuejiao.hou@gmail.com>
2017-09-21 16:08:13 -0700

    Stretch goals Thu Sep 21

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 'use strict';
 
+var i;
 var pointsScored = 0;
+var arrayOfQuestions = [
+  'Does Joy like pineapples on pizza?',
+  'Does Joy have a dog?',
+  'Does Joy have a cat?',
+  'Was Joy born in a barn?',
+  'Is Joy awesome?'];
+
+var arrayOfCorrectAnswers = [
+  'N',
+  'Y',
+  'Y',
+  'N',
+  'Y'];
+
+var arrayOfCorrectAnswers2 = [
+  'NO',
+  'YES',
+  'YES',
+  'NO',
+  'YES'
+];
+
+var arrayOfPossibleResponses = [
+  'You are right!',
+  'You are wrong.'];
+
+var arrayOfUserAnswers = [];
+
+var arrayOfBrowserResults = [
+  'q1result',
+  'q2result',
+  'q3result',
+  'q4result',
+  'q5result'];
 
 // Prompts user for their name. If the user does not enter a value, they will be prompted again.
 var userName = prompt('Hi there! What is your name?');
@@ -13,128 +48,31 @@ document.getElementById('username').innerHTML = userName;
 // Tells user about the game
 alert('Hello, ' + userName + '! Let\'s play a game to see how well you know Joy. Type \"yes\" or \"no\" to the following five questions.');
 
-// Question 1
+function yesNoQuestions () {
+  for (i = 0; i < 5; i++) {
+    var answerNumber = prompt(arrayOfQuestions[i]).toUpperCase();
 
-function function1() {
-  var answer1 = prompt('Does Joy like pineapples on pizza?').toUpperCase();
+    while (answerNumber !== 'Y' && answerNumber !== 'YES' && answerNumber !== 'N' && answerNumber !== 'NO') {
+      answerNumber = prompt('You did not answer in the correct format. ' + arrayOfQuestions[i]).toUpperCase();
+    }
 
-  while (answer1 !== 'Y' && answer1 !== 'YES' && answer1 !== 'N' && answer1 !== 'NO') {
-    answer1 = prompt('You did not answer in the correct format. Please answer with yes or no. Does Joy like pineapples on pizza?').toUpperCase();
+    arrayOfUserAnswers[i] = answerNumber;
+
+    if (answerNumber === arrayOfCorrectAnswers[i] || answerNumber === arrayOfCorrectAnswers2[i]) {
+      alert(arrayOfPossibleResponses[0]);
+      pointsScored++;
+      console.log(arrayOfQuestions[i] + ' ' + userName + ' answered ' + arrayOfUserAnswers[i] + '. The correct answer is ' + arrayOfCorrectAnswers2[i] + '. Congrats!');
+    }
+    else {
+      alert(arrayOfPossibleResponses[1]);
+      console.log(arrayOfQuestions[i] + ' ' + userName + ' answered ' + arrayOfUserAnswers[i] + '. The correct answer is ' + arrayOfCorrectAnswers2[i] + '.');
+    }
+    document.getElementById(arrayOfBrowserResults[i]).innerHTML = answerNumber;
   }
-
-  if (answer1 === 'N' || answer1 === 'NO') {
-    alert('You\'re right, of course!');
-    console.log(userName + ' guessed that Joy does not like pineapple on pizza, because she is not a monster. Correct!');
-    pointsScored++;
-  }
-  else {
-    alert('Seriously? You\'re wrong; Joy has some class.');
-    console.log(userName + ' guessed that Joy likes pineapple on pizza. Gross!');
-  }
-
-  document.getElementById('q1result').innerHTML = answer1;
 }
-
-function1();
-
-// Question 2
-
-function function2() {
-  var answer2 = prompt('Does Joy have a dog?').toUpperCase();
-
-  while (answer2 !== 'Y' && answer2 !== 'YES' && answer2 !== 'N' && answer2 !== 'NO') {
-    answer2 = prompt('You did not answer in the correct format. Please answer with yes or no. Does Joy have a dog?').toUpperCase();
-  }
-
-  if (answer2 === 'Y' || answer2 === 'YES') {
-    alert('You\'re right, she does! She has a labradoodle named Toby.');
-    console.log(userName + ' guessed correctly that Joy does have a dog.');
-    pointsScored++;
-  }
-  else {
-    alert('You are wrong!');
-    console.log(userName + ' guessed that Joy does not have a dog. Wrong!');
-  }
-
-  document.getElementById('q2result').innerHTML = answer2;
-}
-
-function2();
-
-// Question 3
-
-function function3() {
-  var answer3 = prompt('Does Joy have a cat?').toUpperCase();
-
-  while (answer3 !== 'Y' && answer3 !== 'YES' && answer3 !== 'N' && answer3 !== 'NO') {
-    answer3 = prompt('You did not answer in the correct format. Please answer with yes or no. Does Joy have a cat?').toUpperCase();
-  }
-
-  if (answer3 === 'Y' || answer3 === 'YES') {
-    alert('You\'re right, she does! Her cat\'s is named Snowdrop.');
-    console.log(userName + ' guessed correctly that Joy has a cat.');
-    pointsScored++;
-  }
-  else {
-    alert('You are wrong!');
-    console.log(userName + ' guessed that Joy does not have a cat. Wrong!');
-  }
-
-  document.getElementById('q3result').innerHTML = answer3;
-}
-
-function3();
-
-// Question 4
-
-function function4() {
-  var answer4 = prompt('Was Joy born in a barn?').toUpperCase();
-
-  while (answer4 !== 'Y' && answer4 !== 'YES' && answer4 !== 'N' && answer4 !== 'NO') {
-    answer4 = prompt('You did not answer in the correct format. Please answer with yes or no. Was Joy born in a barn?').toUpperCase();
-  }
-
-  if (answer4 === 'N' || answer4 === 'NO') {
-    alert('You\'re right! She was not born in a barn.');
-    console.log(userName + ' guessed correctly that Joy was not born in a barn.');
-    pointsScored++;
-  }
-  else {
-    alert('You jerk, how could you think that??');
-    console.log(userName + ' is a big wrong jerk and guessed that Joy is born in a barn.');
-  }
-
-  document.getElementById('q4result').innerHTML = answer4;
-}
-
-function4();
-
-// Question 5
-
-function function5() {
-  var answer5 = prompt('Is Joy awesome?').toUpperCase();
-
-  while (answer5 !== 'Y' && answer5 !== 'YES' && answer5 !== 'N' && answer5 !== 'NO') {
-    answer5 = prompt('You did not answer in the correct format. Please answer with yes or no. Is Joy awesome?').toUpperCase();
-  }
-
-  if (answer5 === 'Y' || answer5 === 'YES') {
-    alert('Of course, you are correct.');
-    console.log(userName + ' guessed correctly that Joy is awesome.');
-    pointsScored++;
-  }
-  else {
-    alert('You are wrong!');
-    console.log(userName + ' thinks Joy is not awesome!! OMG.');
-  }
-
-  document.getElementById('q5result').innerHTML = answer5;
-}
-
-function5();
+yesNoQuestions();
 
 // Question 6
-
 function function6() {
   var answer6 = Math.round(prompt('How lucky are you? Guess a whole number between 1 and 10! You get four tries.'));
   var randomNumber = Math.floor(Math.random() * 10 + 1);
@@ -181,7 +119,6 @@ function function6() {
 function6();
 
 // Question 7
-
 function function7() {
   var answer7 = prompt('Can you guess one of Joy\'s top three favorite countries to visit? You get six tries!').toUpperCase();
   var topThreeCountries = ['BELIZE', 'MEXICO', 'CHINA'];
@@ -220,7 +157,6 @@ function function7() {
 }
 
 function7();
-
 
 // Displays points earned and a message accordingly
 document.getElementById('points').innerHTML = pointsScored;
